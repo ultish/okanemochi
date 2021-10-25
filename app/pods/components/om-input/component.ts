505cdaee7a8c25e84aad2c6befdbc40241a8ece3
Jimmy <ultish@gmail.com>
2021-10-25 14:27:57 +1100

    ember-css-modules, sass, tailwind

---
+++ app/pods/components/om-input/component.ts
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+
+interface OmInputArgs {}
+
+export default class OmInput extends Component<OmInputArgs> {
+
+
+}
--- app/pods/components/test-one/component.ts
+++
@@ -1,5 +0,0 @@
-import Component from '@glimmer/component';
-
-interface TestOneArgs {}
-
-export default class TestOne extends Component<TestOneArgs> {}
